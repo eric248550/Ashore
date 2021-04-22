bde493ee8b530e79b85b878fb4421649c5b78264
VivianChan1998 <b05901179@ntu.edu.tw>
2021-04-22 14:45:19 +0800

    ashore2 complete

--- ashore2.py
+++ ashore2.py
@@ -1,5 +1,32 @@
 import serial
+import pygame
 from time import sleep
+
+def press_center():
+    print('center')
+    global is_play
+    if is_play:
+        m.pause()
+        is_play = 0
+    else:
+        m.unpause()
+        is_play = 1
+
+def press_prev():
+    print('prev')
+
+def press_forw():
+    print('forw')
+
+def press_volume():
+    global vol
+    for i in range(len(VOL)):
+        if c == VOL[i]:
+            vol = i+1
+            m.set_volume(vol/6)
+            break
+    print('vol')
+    print(vol)
 
 
 START = bytes.fromhex('F2')
@@ -7,17 +34,29 @@ END = bytes.fromhex('2F')
 CENTER = bytes.fromhex('84072f')
 PREV = bytes.fromhex('813f2f')
 FORW = bytes.fromhex('823f2f')
-VOL0 = bytes.fromhex('')
+VOL = [bytes.fromhex('80012f'), bytes.fromhex('80032f'), bytes.fromhex('80072f'),bytes.fromhex('800f2f'), bytes.fromhex('801f2f'), bytes.fromhex('803f2f')]
+'''
 VOL1 = bytes.fromhex('80012f')
 VOL2 = bytes.fromhex('80032f')
 VOL3 = bytes.fromhex('80072f')
 VOL4 = bytes.fromhex('800f2f')
 VOL5 = bytes.fromhex('801f2f')
 VOL6 = bytes.fromhex('803f2f')
+'''
 c = ''
+c_prev = ''
+vol = 1
+is_play = 1
 
+file = "./music.mp3"
+pygame.mixer.init()
+pygame.mixer.music.load(file)
+m = pygame.mixer.music
+m.set_volume(vol/6)
+m.play()
 
 ser = serial.Serial("/dev/ttyUSB0",9600)
+
 print(ser)
 ser.reset_input_buffer()
 print("start")
@@ -26,21 +65,17 @@ while True:
     if ser.inWaiting() > 3:
         first = ser.read()
         if first == START:
-            print("here")
-            # record
             temp = ser.read_until(END)
             print(temp.hex())
+            c_prev = c
             c = temp
-        # passng on
-        # else:
-        #    ser.read_until(END)
         else:
             c = ''
-    if c == CENTER:
-        print("center")
-    elif c == PREV:
-        print("prev")
-    elif c == FORW:
-        print("forw")
-
-    # debounce
+    if c == CENTER && c_prev != CENTER:
+        press_center()
+    elif c == PREV && c_prev != PREV:
+        press_prev()
+    elif c == FORW && c_prev != FORW:
+        press_forw()
+    elif c != ''
+        press_volume()
