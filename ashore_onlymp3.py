457e80ce5061924d8bf5fa0f07409b0c753c9ce7
EricTsai <eric248550@gmail.com>
2021-04-30 05:39:04 +0100

    only mp3 done

--- ashore_onlymp3.py
+++ ashore_onlymp3.py
@@ -27,16 +27,16 @@ def press_forw():
 def press_volume():
     global vol
     for i in range(len(VOL)):
-        if c == VOL[i]:
+        if c2 == VOL[i]:
             vol = i+1
-            m.set_volume(0.5 + vol/12)
+            m.set_volume(vol/6)
             break
     #print('vol')
-    #print(vol)
+    print(vol)
 
 def load_new_file():
-    print(current_file)
     global is_play
+    print(current_file)
     m.stop()
     #m.unload()
     m.load(file[current_file])
@@ -51,14 +51,6 @@ PREV = b'\x81'
 FORW = b'\x82'
 VOL = [b'\x01', b'\x03',b'\x07',b'\x0f',b'\x1f',b'\x2f']
 #VOL2 = [bytes.fromhex('80012f'), bytes.fromhex('80032f'), bytes.fromhex('80072f'),bytes.fromhex('800f2f'), bytes.fromhex('801f2f'), bytes.fromhex('803f2f')]
-'''
-VOL1 = bytes.fromhex('80012f')
-VOL2 = bytes.fromhex('80032f')
-VOL3 = bytes.fromhex('80072f')
-VOL4 = bytes.fromhex('800f2f')
-VOL5 = bytes.fromhex('801f2f')
-VOL6 = bytes.fromhex('803f2f')
-'''
 c = ''
 c_prev = ''
 vol = 6
@@ -70,7 +62,7 @@ current_file = 0
 pygame.mixer.init()
 pygame.mixer.music.load(file[current_file])
 m = pygame.mixer.music
-m.set_volume(0.5 + vol/12)
+m.set_volume(vol/6)
 m.play()
 
 ser = serial.Serial("/dev/ttyUSB0",9600)
@@ -84,16 +76,15 @@ while True:
             c_prev = c
             c = temp
             c2 = ser.read()
-        else:
-            c = ''
-    if c == CENTER and c_prev != CENTER:
-        press_center()
-    elif c == PREV and c_prev != PREV:
-        press_prev()
-    elif c == FORW and c_prev != FORW:
-        press_forw()
-    elif c != '' and c2 != '':
-        press_volume()
+            if c == CENTER and c_prev != CENTER:
+                press_center()
+            elif c == PREV and c_prev != PREV:
+                press_prev()
+            elif c == FORW and c_prev != FORW:
+                press_forw()
+            elif c != '' and c2 != '':
+                press_volume()
 
+    
 
         
